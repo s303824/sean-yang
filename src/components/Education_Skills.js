aae7f924f45935c14f6bdb288be0e0b7906f4f74
Sean Yang <60932078+s303824@users.noreply.github.com>
2024-08-12 18:06:40 -0400

    minor spelling mistake

--- src/components/Education_Skills.js
+++ src/components/Education_Skills.js
@@ -9,7 +9,7 @@ function Education_Skills(){
 
             <Box sx={{ flexDirection:"column" , alignContent:"space-between"}}>
                 <Box sx={{display:"flex", flexDirection:"row" , justifyContent:"space-between"}}>
-                    <Typography variant="h4" fontFamily={'inconsolata'}>Stony Brook Univerity</Typography> 
+                    <Typography variant="h4" fontFamily={'inconsolata'}>Stony Brook University</Typography> 
                     <Typography variant="h5" fontFamily={'inconsolata'}>Graduated 2023</Typography> 
                 </Box>
                 <Typography variant="h5" fontFamily={'inconsolata'}>Bachelor of Science in Computer Science</Typography> 
