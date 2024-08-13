517f160e9854290365518bf30216fea9ea9fc0ca
Sean Yang <60932078+s303824@users.noreply.github.com>
2024-08-13 00:33:13 -0400

    badge look change

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import { Box, Typography } from "@mui/material";
 export default function Badge({subject}) {
     return(
     <Box className='badge'>
-        <Typography fontFamily={'inconsolata'}>
+        <Typography fontFamily={'inconsolata'} variant="h7">
             {subject}
         </Typography>
     </Box>
