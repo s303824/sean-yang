1a2ed076f5d830b7b4d6fff6004e3f4a99ce7f1e
Sean Yang <60932078+s303824@users.noreply.github.com>
2024-08-14 23:48:41 -0400

    image size fix

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,12 +3,18 @@ import logo from '../images/profile.jpg';
 import '../App.css';
 import CircleImage from "./CircleImage";
 import resume from '../Sean_Yang_resume.pdf'
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function AboutSection(){
+    const matches = useMediaQuery('(min-width:632px)');
+    const isWrap = matches ? "nowrap": "wrap";
+    const isMarginApplied = matches ? 4: 0;
+
+
     return(
-        <Box className="about" sx={{ flexDirection:"row" , padding:4, justifyContent:"space-around"}}>
-            <CircleImage image_file={logo}/>
-            <Box sx={{marginLeft:2}}>
+        <Box className="about" sx={{ display:"flex", flexDirection:"row" , flexWrap:isWrap, padding:4, justifyContent:"space-around"}}>
+            <CircleImage image_file={logo} />
+            <Box sx={{marginLeft:isMarginApplied}}>
                 <Typography className="subtitle" fontFamily={'inconsolata'} variant="h3">Hello!</Typography>
                 <Typography variant="h6" fontFamily={'inconsolata'}>
                     My name is Sean Yang, a software engineer specializing in game programming (both systems and gameplay) and web development. I enjoy tackling new challenges and would love to talk about job opportunities.
--- src/components/CircleImage.js
+++ src/components/CircleImage.js
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material";
 import '../App.css';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 function CircleImage({image_file}){
+    const matches = useMediaQuery('(min-width:632px)');
+    const img_size = matches ? '250vw': '200vh';
+
     return(
     <Box className="image-container">
-        <img src={image_file} alt="profile pic"/>
+        <img src={image_file} height={img_size} alt="profile pic"/>
     </Box>
 )
 }
