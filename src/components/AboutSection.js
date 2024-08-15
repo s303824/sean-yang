7b11d417fbb46f23a787a8e2c65d1b221d4791f7
Sean Yang <60932078+s303824@users.noreply.github.com>
2024-08-14 22:45:34 -0400

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -11,7 +11,7 @@ function AboutSection(){
             <Box sx={{marginLeft:2}}>
                 <Typography className="subtitle" fontFamily={'inconsolata'} variant="h3">Hello!</Typography>
                 <Typography variant="h6" fontFamily={'inconsolata'}>
-                    My name is Sean Yang, a software engineer specializing in game programming (both systems and gameplay) and web development. I enjoy tackling new challenges and would love to talk about any job opportunities.
+                    My name is Sean Yang, a software engineer specializing in game programming (both systems and gameplay) and web development. I enjoy tackling new challenges and would love to talk about job opportunities.
                 </Typography> 
                 <Button href={resume} sx={{borderRadius: '0%', border: 'solid',color: 'white',borderColor: 'white',  cursor: 'pointer !important', width:'256px', marginTop:4, marginBottom:4, fontFamily:"inconsolata"}} >
                           My Resume
